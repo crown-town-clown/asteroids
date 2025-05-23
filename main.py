1acd263822d8077035df952b7259340f19eb42ac
crown-town-clown <63020400+crown-town-clown@users.noreply.github.com>
2025-05-23 11:36:57 -0700

    add circle shapes

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	game_clock = pygame.time.Clock()
+	dt = 0
 
 	while True:
 		screen.fill(000000)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		dt = game_clock.tick(60) / 1000
 		pygame.display.flip()
 
 if __name__ == "__main__":
