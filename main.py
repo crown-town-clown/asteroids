5b8b26a4895ec249c567e2a5bc91fc2897caee22
crown-town-clown <63020400+crown-town-clown@users.noreply.github.com>
2025-05-23 12:28:54 -0700

    added player, rotate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -12,11 +13,15 @@ def main():
 	game_clock = pygame.time.Clock()
 	dt = 0
 
+	player1 = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
 	while True:
 		screen.fill(000000)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		player1.draw(screen)
+		player1.update(dt)
 		dt = game_clock.tick(60) / 1000
 		pygame.display.flip()
 
